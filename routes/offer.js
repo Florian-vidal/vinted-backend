6b077c5bb3690ea4da6cc6d064b8e393ddf46379
ezekiel <florian.vidal@lapiscine.pro>
2025-09-02 21:38:35 +0200

    first commit

--- routes/offer.js
+++ routes/offer.js
@@ -1,10 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const fileUpload = require("express-fileupload");
+const convertToBase64 = require("../utils/convertBase64");
+const cloudinary = require("cloudinary").v2;
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
+const Offer = require("../models/Offer");
+
+// Route publish
 router.post(
   "/offer/publish",
   isAuthenticated,
   fileUpload(),
   async (req, res) => {
     try {
-      // ✅ On récupère les champs envoyés par le frontend
       const { title, description, price, brand, size, condition, color, city } =
         req.body;
 
@@ -28,7 +37,6 @@ router.post(
         owner: req.user,
       });
 
-      // ✅ Gestion de l’image
       if (req.files?.picture) {
         const convertedPicture = convertToBase64(req.files.picture);
         const uploadResponse = await cloudinary.uploader.upload(convertedPicture);
@@ -43,3 +51,59 @@ router.post(
     }
   }
 );
+
+// Liste des offres
+router.get("/offers", async (req, res) => {
+  try {
+    let skip = 0;
+    let limit = 10;
+    const filters = {};
+
+    if (req.query.title) {
+      filters.product_name = new RegExp(req.query.title, "i");
+    }
+    if (req.query.priceMax) {
+      filters.product_price = { $lte: Number(req.query.priceMax) };
+    }
+    if (req.query.priceMin) {
+      filters.product_price = {
+        ...filters.product_price,
+        $gte: Number(req.query.priceMin),
+      };
+    }
+
+    const sortedObject = {};
+    if (req.query.sort) {
+      sortedObject.product_price = req.query.sort.replace("price-", "");
+    }
+    if (req.query.page) {
+      skip = (req.query.page - 1) * limit;
+    }
+
+    const offers = await Offer.find(filters)
+      .populate("owner", "account")
+      .sort(sortedObject)
+      .limit(limit)
+      .skip(skip);
+
+    return res.status(200).json(offers);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// Détail d’une offre
+router.get("/offers/:id", async (req, res) => {
+  try {
+    const offerDetails = await Offer.findById(req.params.id).populate("owner");
+    if (offerDetails) {
+      return res.status(200).json(offerDetails);
+    } else {
+      return res.status(400).json({ message: "This ID doesn't exist" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
